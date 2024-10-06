a34ab3fa7360b9a2c89b758728c3c3a32d86fddd
Uscout07 <pantudit007@gmail.com>
2024-10-06 19:54:06 +0530

    pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  assetPrefix: "./",
+  trailingSlash: true,
   images: {
     remotePatterns: [
       {
-        output: "export",
-        assetPrefix: "./",
-        trailingSlash: true,
         protocol: "https",
         hostname: "images.pexels.com",
         port: "",
